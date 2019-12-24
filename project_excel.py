ec2f368c6652f5493eaa949ee886658fb09ec66e
Dakshayani04 <59014959+Dakshayani04@users.noreply.github.com>
2019-12-24 17:52:31 +0530

    Update version

--- project_excel.py
+++ project_excel.py
@@ -22,9 +22,16 @@ df2 = pd.read_excel('C:\\Users\\dakshayani\\Desktop\\Book3.xlsx', sheet_name='Mo
 
 '''
 
-def get_employee_id():
-    get_employee_id = pd.DataFrame(df1,columns=["Employee"])
+def get_employee_detail():
+    get_employee_id = pd.DataFrame(df1,columns=["Employee", "Emp ID","Exa Name Byd"])
     return get_employee_id
+
+'''def get_employee_mailID():
+    # get the mail ID from the masterdata
+    mailID = pd.DataFrame(df1,columns=["Emp ID"])
+    for i, j in mailID.iterrows():
+        recipients_mailID = j[0]
+        #print(recipients_mailID)'''
 
 '''
     search the employee Id exist in AC sheet and extract the
@@ -32,7 +39,7 @@ def get_employee_id():
 
 '''
 
-def search_employee_id(check_employee_id):
+def search_employee_id(check_employee_id, mailID, name):
     my_result = df2.iloc[:, 10]
     #print(check_employee_id)
     for i in my_result:
@@ -40,10 +47,14 @@ def search_employee_id(check_employee_id):
             for row in range(df2.shape[0]): # df is the DataFrame
                 for col in range(df2.shape[1]):
                     if df2.iat[row,col] == i:
-                        print(row, col)
                         row_data = row + 9
                         test = df2.iloc[row+2:row+9,0:col-2]
-                        send_mail(test)
+                        cols = [0,2]
+                        result = test.drop(test.columns[cols], axis = 1)
+                        result_row = result.drop(result.index[[0,3,4,5]])
+                        result_row.columns = [' ', df2.iloc[10,3], df2.iloc[10,4],df2.iloc[10,5],df2.iloc[10,6],df2.iloc[10,7]]
+                        print(result_row)
+                        send_mail(result_row, mailID, name)
     else:
         pass
 
@@ -53,38 +64,39 @@ def search_employee_id(check_employee_id):
     function to send the mail
 
 '''
-def send_mail(data_to_mail):
-    # get the mail ID from the masterdata
-    mailID = pd.DataFrame(df1,columns=["Emp ID"])
-    for i, j in mailID.iterrows():
-        recipients_mailID = j[0]
-        #print(recipients_mailID)
-        from_email = "dakshayani.r@exa-ag.com"
-        msg = MIMEMultipart()
-        msg['Subject'] = "Employee Attendence"
-        msg['From'] = 'dakshayani.r@exa-ag.com'
-        html = """\
-        <html>
-          <head></head>
-          <body>
-            {0}
-          </body>
-        </html>
-        """.format(data_to_mail.to_html())
 
-        part1 = MIMEText(html, 'html')
-        msg.attach(part1)
-        server = smtplib.SMTP('smtp.office365.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login(from_email, "password")
-        server.sendmail(msg['From'], recipients_mailID , msg.as_string())
+
+def send_mail(data_to_mail, recipients_mailID,name):
+    print(recipients_mailID)
+    from_email = "dakshayani.r@exa-ag.com"
+    msg = MIMEMultipart()
+    msg['Subject'] = "Employee Attendence"
+    msg['From'] = 'dakshayani.r@exa-ag.com'
+    html = """\
+    <html>
+      <head></head>
+      <body>
+        {0}
+        {1}
+      </body>
+    </html>
+    """.format(name, data_to_mail.to_html())
+
+    part1 = MIMEText(html, 'html')
+    msg.attach(part1)
+    server = smtplib.SMTP('smtp.office365.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(from_email, "Password")
+    server.sendmail(msg['From'], recipients_mailID , msg.as_string())
 
 
 if __name__ == '__main__':
-    employee_id = get_employee_id()
+    employee_id = get_employee_detail()
     for i, j in employee_id.iterrows():
-        data = j[0][3:6]
-        #result = list(data.split("\n"))
-        search_employee_id(data)
+        employee_id = j[0][3:6]
+        employee_mailID = j[1]
+        employee_name = j[2]
+        #print(employee_name)
+        search_employee_id(employee_id, employee_mailID, employee_name)
